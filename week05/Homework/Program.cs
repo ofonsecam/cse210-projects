0955e96fd81dcde39a39bc8cf6659b62a8f45f84
ofonsecam <fonsecaoscarestudios@gmail.com>
2026-06-02 19:55:48 -0500

    W05 homework completed!

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,22 @@
 using System;
-
-class Program
+namespace Homework
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        static void Main(string[] args)
+        {
+            Assignment baseAssignment = new Assignment("Samuel Bennett", "Multiplication");
+            Console.WriteLine(baseAssignment.GetSummary());
+            Console.WriteLine(); // Espacio en consola
+
+            MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "3-10, 20-21");
+            Console.WriteLine(mathAssignment.GetSummary());
+            Console.WriteLine(mathAssignment.GetHomeworkList());
+            Console.WriteLine();
+
+            WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+            Console.WriteLine(writingAssignment.GetSummary());
+            Console.WriteLine(writingAssignment.GetWritingInformation());
+        }
     }
 }
