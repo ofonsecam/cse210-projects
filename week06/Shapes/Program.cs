133379f2dfe2d9556611701bc2d7c59b5c667d77
ofonsecam <fonsecaoscarestudios@gmail.com>
2026-06-09 21:51:31 -0500

    W06 learning activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace Shapes
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        static void Main(string[] args)
+        {
+            List<Shape> shapesList = new List<Shape>();
+
+            Square smallSquare = new Square("Red", 4.0);
+            Rectangle mediumRectangle = new Rectangle("Blue", 5.0, 3.0);
+            Circle plateCircle = new Circle("Green", 2.0);
+
+            shapesList.Add(smallSquare);
+            shapesList.Add(mediumRectangle);
+            shapesList.Add(plateCircle);
+
+            Console.WriteLine("---Results of Area Calculations---");
+            foreach (Shape shape in shapesList)
+            {
+                string color = shape.GetColor();
+                double area = shape.GetArea();
+
+                string shapeType = shape.GetType().Name;
+                Console.WriteLine($"Form:{shapeType} ({color}): Area = {area:F2}");
+            }
+        }
     }
 }
